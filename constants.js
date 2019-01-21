0e24cff2bb9403e285e47087cf9dca729ef5c6fd
wuhaosky <wuhaosky@163.com>
2019-01-21 23:05:39 +0800

    pm2输入文件的默认路径

--- constants.js
+++ constants.js
@@ -5,16 +5,21 @@
 
 var p = require('path');
 
-DEFAULT_FILE_PATH = p.resolve(process.env.HOME, '.pm2');
+DEFAULT_FILE_PATH = p.resolve(process.env.HOME, '.pm2'); // "/Users/wuhao/.pm2"
+
+// process.env.HOME  "/Users/wuhao"
 
 module.exports = {
   DEFAULT_FILE_PATH : DEFAULT_FILE_PATH,
-  PM2_LOG_FILE_PATH : p.join(p.resolve(process.env.HOME, '.pm2'), 'pm2.log'),
-  DEFAULT_PID_PATH  : p.join(DEFAULT_FILE_PATH, 'pids'),
-  DEFAULT_LOG_PATH  : p.join(DEFAULT_FILE_PATH, 'logs'),
-  DUMP_FILE_PATH    : p.join(DEFAULT_FILE_PATH, 'dump.pm2'),
+  PM2_LOG_FILE_PATH : p.join(p.resolve(process.env.HOME, '.pm2'), 'pm2.log'), // "/Users/wuhao/.pm2/pm2.log"
+  DEFAULT_PID_PATH  : p.join(DEFAULT_FILE_PATH, 'pids'), // "/Users/wuhao/.pm2/pids"
+  DEFAULT_LOG_PATH  : p.join(DEFAULT_FILE_PATH, 'logs'), // "/Users/wuhao/.pm2/logs"
+  DUMP_FILE_PATH    : p.join(DEFAULT_FILE_PATH, 'dump.pm2'), // "/Users/wuhao/.pm2/dump.pm2"
   DAEMON_BIND_ADDR  : 'localhost:6666',
-  DEBUG             : false,
+  DEBUG             : true,
   WEB_INTERFACE     : 9615,
   MS_TO_STOP_SCRIPT : 1000
 };
+
+
+
